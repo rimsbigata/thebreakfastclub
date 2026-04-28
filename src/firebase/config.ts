c316dacfc970ec90744f93c940b00219acc3ae65
John Rimuel Bigata <jrbigata@gmail.com>
2026-04-28 14:46:27 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,6 @@ export const firebaseConfig = {
   "appId": "1:380629825062:web:e595813b55ac4626ddd8e7",
   "apiKey": "AIzaSyAKGYRP8SjvCq-hT2w5yNDIJEOhjaJGvw8",
   "authDomain": "studio-8289009920-31c2b.firebaseapp.com",
-  "storageBucket": "studio-8289009920-31c2b.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "380629825062"
 };
